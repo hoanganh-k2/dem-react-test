a42ee88ae25b664cd767714f8c3a3d043c61b300
Hoang Anh <anhh3642@gmail.com>
2025-04-05 00:36:04 +0700

    DOM events and setState

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,10 +6,37 @@ class MyComponent extends React.Component {
         address: "Ha Noi",
         age: 22,
     };
+
+    handleOnClick = (event) => {
+        console.log(">> click me", this.state.name);
+        this.setState({
+            name: "Hoang Anh",
+            age: Math.floor(Math.random() * 100 + 1),
+        });
+    };
+
+    handleOnMouseOver = (event) => {
+        console.log(event.pageX);
+    };
+
     render() {
         return (
             <div>
                 My name is {this.state.name} and from {this.state.address}
+                <button
+                    onClick={(event) => {
+                        this.handleOnClick(event);
+                    }}
+                >
+                    Click me!
+                </button>
+                <button
+                    onMouseOver={(event) => {
+                        this.handleOnMouseOver(event);
+                    }}
+                >
+                    Hover me!
+                </button>
             </div>
         );
     }
