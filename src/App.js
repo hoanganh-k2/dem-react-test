f5a5fb7a03ff573032a684da7d6364f77aa85e63
Hoang Anh <anhh3642@gmail.com>
2025-04-19 18:10:49 -0700

    Điều hướng trang với thẻ Links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.scss";
 import Headers from "./components/Header/Header";
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app-container">
       <Headers />
+      <div>
+        test link
+        <Link to="/users">User</Link>
+        <Link to="/admins">Admin</Link>
+      </div>
     </div>
   );
 }
