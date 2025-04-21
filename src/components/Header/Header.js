cd44a1db3b531cef7c7524d349b195beb36c57ac
Hoang Anh <anhh3642@gmail.com>
2025-04-21 10:49:20 -0700

    Design Homepage and New Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -26,11 +26,13 @@ const Header = () => {
             </NavLink>
           </Nav>
           <Nav>
-            <NavDropdown title="Setting" id="basic-nav-dropdown">
+            <button className="btn-login">Log in</button>
+            <button className="btn-signup">Sign up</button>
+            {/* <NavDropdown title="Setting" id="basic-nav-dropdown">
               <NavDropdown.Item>Log In</NavDropdown.Item>
               <NavDropdown.Item>Log Out</NavDropdown.Item>
               <NavDropdown.Item>Profile</NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
