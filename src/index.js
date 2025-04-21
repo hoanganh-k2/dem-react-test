bc84e0640e3b249f1c9e9feeb80649fcc780b0ae
Hoang Anh <anhh3642@gmail.com>
2025-04-21 04:02:42 -0700

    Active link and Index routes

--- src/index.js
+++ src/index.js
@@ -8,19 +8,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import User from "./components/User/User";
 import Admin from "./components/Admin/Admin";
+import HomePage from "./components/Home/HomePage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App />} >
-                    <Route path="/users" element={<User />} />
-                    <Route path="/admins" element={<Admin />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    </Provider>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="/users" element={<User />} />
+          <Route path="/admins" element={<Admin />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
